badc7027fed1910bcb07048fe54da1f9efe24104
hadeergamal1 <hadeer.gamal@myvrapeutic.com@users.noreply.github.com>
2021-05-05 04:32:41 +0200

    close #10

---
+++ dist/components/Selectnpc.d.ts
@@ -0,0 +1,14 @@
+import { ComponentWrapper } from '../essential/aframe-wrapper';
+interface selectnpcSchema {
+}
+export declare class selectnpc extends ComponentWrapper<selectnpcSchema> {
+    constructor();
+    init(): void;
+    update(): void;
+    play(): void;
+    pause(): void;
+    tick(): void;
+    remove(): void;
+    destroy(): void;
+}
+export {};
