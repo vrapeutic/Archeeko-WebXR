5b648355424f0437faba56f58aa94c32f6af1834
hadeergamal1 <hadeer.gamal@myvrapeutic.com@users.noreply.github.com>
2021-06-29 15:00:51 +0200

    quest hand

--- dist/index.js
+++ dist/index.js
@@ -12877,7 +12877,7 @@ var shoot = /** @class */function (_super) {
         var randomBulletCounter = document.querySelector('#bulletCounter').getAttribute('value');
         var newforce = new CANNON.Vec3(0, 0, 2);
         var bullet = document.getElementById('bullet');
-        document.querySelector('#onHand').addEventListener('click', function () {
+        document.getElementById('onHand').addEventListener('click', function () {
             var currentBullet = document.createElement('a-gltf-model');
             currentBullet.setAttribute('src', '#bullet1');
             currentBullet.setAttribute('id', 'bullet');
